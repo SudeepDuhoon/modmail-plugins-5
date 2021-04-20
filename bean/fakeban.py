7af1b9cb2ef9450ec457c5854ea4bb2dfb5c35ad
izutsumi <76601426+izutsumi@users.noreply.github.com>
2021-04-20 02:46:14 -0400

    Add files via upload

---
+++ bean/fakeban.py
@@ -0,0 +1,24 @@
+import discord
+from discord.ext import commands
+
+class bean(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        self.color = self.bot.main_color
+        
+    @commands.command(name="pban", aliases=['fban'])
+    async def bean(self, ctx, member: discord.Member = None, reason = None):
+        await ctx.message.delete()
+        if not member:
+            embed = discord.Embed(title="Error", description="Please provide a user to bean!", color=self.bot.error_color)
+            await ctx.send(embed=embed)
+        else:
+            if not reason:
+                embed = discord.Embed(title="Ban", description=f"{member.mention} Has been beaned!", color=self.color)
+                await ctx.send(embed=embed)
+            else:
+                embed = discord.Embed(title="Ban", description=f"{member.mention} Has been beaned for {reason}!", color=self.color)
+                await ctx.send(embed=embed)           
+        
+def setup(bot):
+    bot.add_cog(bean(bot))
