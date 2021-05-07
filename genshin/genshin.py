27fb0fd8b7c8b52c4e96cba9d6e44a38c7aedb74
izutsumi <76601426+izutsumi@users.noreply.github.com>
2021-05-07 05:12:02 -0400

    Update genshin.py

--- genshin/genshin.py
+++ genshin/genshin.py
@@ -1,22 +1,26 @@
-import os
 import discord
-import requests
-import json
 from discord.ext import commands
 
 class Genshin(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-
-    @commands.command()
-    async def hello(self, ctx, *, member: discord.Member = None):
-        """Says hello"""
-        member = member or ctx.author
-        if self._last_member is None or self._last_member.id != member.id:
-            await ctx.send('Hello {0.name}~'.format(member))
+        self.color = self.bot.main_color
+        
+    @commands.command(name="testing", aliases=['test'])
+    async def bean(self, ctx, member: discord.Member = None, reason = None):
+        await ctx.message.delete()
+        if not member:
+            embed = discord.Embed(title="Error", description="Please provide a user to bean!", color=self.bot.error_color)
+            await ctx.send(embed=embed)
         else:
-            await ctx.send('Hello {0.name}... This feels familiar.'.format(member))
-        self._last_member = member
-    
+            if not reason:
+                embed = discord.Embed(title="Bean | case 420", description=f"{member.mention} Has been beaned!", color=self.color)
+                embed.set_thumbnail(url="https://cdn.discordapp.com/emojis/819994849838366771.png")
+                await ctx.send(embed=embed)
+            else:
+                embed = discord.Embed(title="Bean | case 69", description=f"{member.mention} Has been beaned for {reason}!", color=self.color)
+                embed.set_thumbnail(url="https://cdn.discordapp.com/emojis/819994849838366771.png")
+                await ctx.send(embed=embed)           
+        
 def setup(bot):
     bot.add_cog(Genshin(bot))
