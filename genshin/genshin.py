e304a808aca6ea507e788105e1f1ea12eaa10a19
izutsumi <76601426+izutsumi@users.noreply.github.com>
2021-05-07 05:32:30 -0400

    Update genshin.py

--- genshin/genshin.py
+++ genshin/genshin.py
@@ -8,24 +8,49 @@ class Genshin(commands.Cog):
         self.bot = bot
         self.color = self.bot.main_color
         
-    @commands.command(name="testing", aliases=['test'])
-    async def bean(self, ctx, member: discord.Member = None, reason = None):
-        await ctx.message.delete()
-        if not member:
-            embed = discord.Embed(title="Error", description="Please provide a user to bean!", color=self.bot.error_color)
-            await ctx.send(embed=embed)
-        else:
-            if not reason:
-                embed = discord.Embed(title="Bean | case 420", description=f"{member.mention} Has been beaned!", color=self.color)
-                embed.set_thumbnail(url="https://cdn.discordapp.com/emojis/819994849838366771.png")
-                await ctx.send(embed=embed)
-            else:
-                embed = discord.Embed(title="Bean | case 69", description=f"{member.mention} Has been beaned for {reason}!", color=self.color)
-                embed.set_thumbnail(url="https://cdn.discordapp.com/emojis/819994849838366771.png")
-                await ctx.send(embed=embed)          
+@commands.command(name="weapon",aliases=["wpn"])
+async def _weapon(ctx, *, arg=None): 
+    if arg == None:
+      def listToString(wl):  
+        str1 = "\n" 
+        return (str1.join(wl))  
+      wplist = requests.get('https://api.genshin.dev/weapons').text
+      wl = json.loads(wplist)  
+      embed = discord.Embed(title="Weapon List", description=listToString(wl).title().replace("-", " "))
+      await ctx.send(embed=embeded)
+
+      ## # Same as artifact, but Weapons hold to many data, so it only shown a part of it
+      # for i in wl:
+      #   response = requests.get(wp.format(i)).text
+      #   data = json.loads(response)
+      #   embeded.add_field(name=i.title().replace("-", " "), value="Type: {}".format(data['type']), inline=True)
+      # await ctx.send(embed=embeded)
+
+    elif arg != None:
+      arg = arg.replace(" ", "-").lower()
+      wplist = requests.get('https://api.genshin.dev/weapons').text
+      wl = json.loads(wplist)  
+      if arg in wl:
+        response = requests.get(wp.format(arg)).text
+        data = json.loads(response)
+        embed = discord.Embed(title=data['name'])
+        print(data['name'])
+        embed.set_thumbnail(url=imgw.format(data['name'].replace(" ", "_")))
+        embed.add_field(name="Type", value=data['type'], inline=True)
+        embed.add_field(name="Base ATK", value=data['baseAttack'], inline=True) 
+        rrt = int(data['rarity'])
+        strg = "".join([" :star: ".format(x, x*2) for x in range(rrt)])
+        embed.add_field(name="Rarity", value=strg, inline=True)
+        embed.add_field(name="Sub Stat", value=data['subStat'], inline=True)
+        embed.add_field(name="Where to Get", value=data['location'], inline=True)
+        embed.add_field(name="Passive: {}".format(data['passiveName']), value=data['passiveDesc'], inline=False)
+        await ctx.send(embed=embed)
+      else:
+        await ctx.send("{} not Found!".format(arg).title().replace("-", " "))
+       
                 
     @commands.command(name="yuri3",aliases=["gaytime3"])
-    async def gaytime(self, ctx):
+    async def _gaytime(self, ctx):
         """
         Retrieves a random photo of Yae Sakura and Kallen Kaslana.
         """
