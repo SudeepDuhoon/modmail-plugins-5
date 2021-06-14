c93270c92ae91b5439cf32cf086be8eb073e10e3
izutsumi <76601426+izutsumi@users.noreply.github.com>
2021-06-14 02:41:59 -0400

    Update daily.py

--- daily/daily.py
+++ daily/daily.py
@@ -4,7 +4,6 @@ import discord
 class FAQ(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.color = self.bot.main_color
 
     @commands.command()
     async def faq(self, ctx):
