dbb953df8b5c2caf6a44129c50045de97fc59a8f
izutsumi <76601426+izutsumi@users.noreply.github.com>
2021-06-14 02:25:35 -0400

    Update daily.py

--- daily/daily.py
+++ daily/daily.py
@@ -1,15 +1,18 @@
 from discord.ext import commands
 import discord
 
-class Help(commands.Cog):
+class FAQ(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
     @commands.command()
-    async def help(self, ctx):
+    async def faq(self, ctx):
+        embed = discord.Embed(color=15383739)
+        embed = discord.Embed(title="gay", description="wow im gay")
+        await ctx.send(embed=embed)
         embed = discord.Embed(color=15383739)
         embed = discord.Embed(title="gay", description="wow im gay")
         await ctx.send(embed=embed)
 
 def setup(bot):
-    bot.add_cog(Help(bot))
+    bot.add_cog(FAQ(bot))
