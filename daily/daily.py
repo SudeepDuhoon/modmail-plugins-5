b083586392fec48330cc123b30129480502a1232
izutsumi <76601426+izutsumi@users.noreply.github.com>
2021-06-14 01:57:49 -0400

    Update daily.py

--- daily/daily.py
+++ daily/daily.py
@@ -1,4 +1,5 @@
 from discord.ext import commands
+from datetime import date, datetime
 import discord
 
 class Daily(commands.Cog):
@@ -7,8 +8,13 @@ class Daily(commands.Cog):
 
     @commands.command()
     async def daily(self, ctx):
+    if datetime.today().isoweekday() == 1:
         embed = discord.Embed(color=15383739)
         embed=discord.Embed(title="gay", description="wow im gay")
+        await ctx.send(embed=embed)
+    else:
+        embed = discord.Embed(color=15383739)
+        embed=discord.Embed(title="it is monday", description="wow im gay")
         await ctx.send(embed=embed)
 
 def setup(bot):
