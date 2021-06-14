112150822d1d80a083d44b0b8444db680a5576db
izutsumi <76601426+izutsumi@users.noreply.github.com>
2021-06-14 02:02:56 -0400

    Update daily.py

--- daily/daily.py
+++ daily/daily.py
@@ -10,12 +10,7 @@ class Daily(commands.Cog):
     async def daily(self, ctx):
         embed = discord.Embed(color=15383739)
         embed = discord.Embed(title="gay", description="wow im gay")
-    if datetime.today().isoweekday() == 1:
         await ctx.send(embed=embed)
-    else:
-        if not datetime.today().isoweekday() == 1:
-        embed = discord.Embed(title="Error", description="Please provide a user to bean!")
-        await self.ctx.send(embed=)
 
 def setup(bot):
     bot.add_cog(Daily(bot))
