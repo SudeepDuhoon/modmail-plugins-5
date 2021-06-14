923b670d5fed67dddb7cda0b6722ec4d137e457e
izutsumi <76601426+izutsumi@users.noreply.github.com>
2021-06-14 15:58:53 -0400

    Update daily.py

--- daily/daily.py
+++ daily/daily.py
@@ -10,7 +10,9 @@ class Daily(commands.Cog):
     async def daily(self, ctx):
         if datetime.today().isoweekday() == 0:
             # it is Sunday
-            embed = discord.Embed(title="Sunday", description="It is Sunday.")
+            embed = discord.Embed(title="Today's Dailies (Sunday)", colour=discord.Colour(0xeabcbb))
+            embed.set_image(url="https://cdn.discordapp.com/attachments/815386324658814976/820723149413941369/Sunday.png")
+            embed.set_footer(text="Dailies reset at 4AM. Check #server·status for a countdown.")
             await ctx.send(embed=embed)
         if datetime.today().isoweekday() == 1:
             # it is Monday
@@ -20,24 +22,32 @@ class Daily(commands.Cog):
             await ctx.send(embed=embed)
         if datetime.today().isoweekday() == 2:
             # it is Tuesday
-            embed = discord.Embed(title="Tuesday", colour=discord.Colour(0x51c47a), description="It is Tuesday.")
+            embed = discord.Embed(title="Today's Dailies (Tuesday)", colour=discord.Colour(0xeabcbb))
+            embed.set_image(url="https://cdn.discordapp.com/attachments/815386324658814976/846913736601894912/Tuesday.png")
+            embed.set_footer(text="Dailies reset at 4AM. Check #server·status for a countdown.")
             await ctx.send(embed=embed)
         if datetime.today().isoweekday() == 3:
             # it is Wednesday
-            embed = discord.Embed(title="Wednesday", colour=discord.Colour(0x51c47a), description="It is Wednesday.")
+            embed = discord.Embed(title="Today's Dailies (Wednesday)", colour=discord.Colour(0xeabcbb))
+            embed.set_image(url="https://cdn.discordapp.com/attachments/815386324658814976/847369307071316029/Wednesday.png")
+            embed.set_footer(text="Dailies reset at 4AM. Check #server·status for a countdown.")
             await ctx.send(embed=embed)
         if datetime.today().isoweekday() == 4:
             # it is Thursday
-            embed = discord.Embed(title="Thursday", colour=discord.Colour(0x51c47a), description="It is Thursday.")
+            embed = discord.Embed(title="Today's Dailies (Thursday)", colour=discord.Colour(0xeabcbb))
+            embed.set_image(url="https://cdn.discordapp.com/attachments/815386324658814976/852729859125018654/Thursday.png")
+            embed.set_footer(text="Dailies reset at 4AM. Check #server·status for a countdown.")
             await ctx.send(embed=embed)
         if datetime.today().isoweekday() == 5:
             # it is Friday
-            embed = discord.Embed(title="Friday", colour=discord.Colour(0x51c47a), description="It is Friday.")
-            await ctx.send(embed=embed)
+            embed = discord.Embed(title="Today's Dailies (Friday)", colour=discord.Colour(0xeabcbb))
+            embed.set_image(url="https://cdn.discordapp.com/attachments/815386324658814976/847671599669706762/Friday.png")
+            embed.set_footer(text="Dailies reset at 4AM. Check #server·status for a countdown.")
         if datetime.today().isoweekday() == 6:
-            # it is Friday
-            embed = discord.Embed(title="Saturday", colour=discord.Colour(0x51c47a), description="It is Saturday.")
-            await ctx.send(embed=embed)   
+            # it is Saturday
+            embed = discord.Embed(title="Today's Dailies (Saturday)", colour=discord.Colour(0xeabcbb))
+            embed.set_image(url="https://cdn.discordapp.com/attachments/815386324658814976/852729859125018654/Thursday.png")
+            embed.set_footer(text="Dailies reset at 4AM. Check #server·status for a countdown.")
 
 def setup(bot):
     bot.add_cog(Daily(bot))
