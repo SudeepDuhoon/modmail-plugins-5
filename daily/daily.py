a22de050093ed4c782701268c4540fcc4b8bb906
izutsumi <76601426+izutsumi@users.noreply.github.com>
2021-06-14 02:59:10 -0400

    Update daily.py

--- daily/daily.py
+++ daily/daily.py
@@ -10,11 +10,31 @@ class FAQ(commands.Cog):
     async def faq(self, ctx):
         if datetime.today().isoweekday() == 0:
             # it is Sunday
-            embed = discord.Embed(title="gay", description="wow im gay")
+            embed = discord.Embed(title="Sunday", description="It is Sunday.")
             await ctx.send(embed=embed)
         if datetime.today().isoweekday() == 1:
             # it is Monday
-            embed = discord.Embed(title="Tuesday", colour=discord.Colour(0x51c47a), description="boob")
+            embed = discord.Embed(title="Monday", colour=discord.Colour(0x51c47a), description="It is Monday.")
+            await ctx.send(embed=embed)
+        if datetime.today().isoweekday() == 2:
+            # it is Tuesday
+            embed = discord.Embed(title="Tuesday", colour=discord.Colour(0x51c47a), description="It is Tuesday.")
+            await ctx.send(embed=embed)
+        if datetime.today().isoweekday() == 3:
+            # it is Wednesday
+            embed = discord.Embed(title="Wednesday", colour=discord.Colour(0x51c47a), description="It is Wednesday.")
+            await ctx.send(embed=embed)
+        if datetime.today().isoweekday() == 4:
+            # it is Thursday
+            embed = discord.Embed(title="Thursday", colour=discord.Colour(0x51c47a), description="It is Thursday.")
+            await ctx.send(embed=embed)
+        if datetime.today().isoweekday() == 5:
+            # it is Friday
+            embed = discord.Embed(title="Friday", colour=discord.Colour(0x51c47a), description="It is Friday.")
+            await ctx.send(embed=embed)
+        if datetime.today().isoweekday() == 6:
+            # it is Friday
+            embed = discord.Embed(title="Saturday", colour=discord.Colour(0x51c47a), description="It is Saturday.")
             await ctx.send(embed=embed)   
 
 def setup(bot):
