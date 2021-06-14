97d29b3882d8e5abcf35989bb61009f853dab377
izutsumi <76601426+izutsumi@users.noreply.github.com>
2021-06-14 01:37:34 -0400

    Add files via upload

---
+++ daily/daily.py
@@ -0,0 +1,15 @@
+from discord.ext import commands
+import discord
+
+class Daily(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def daily(self, ctx, *, message):
+        embed = discord.Embed(color=15383739)
+        embed=discord.Embed(title="gay", description="wow im gay")
+        await ctx.send(embed=embed)
+
+def setup(bot):
+    bot.add_cog(Daily(bot))
