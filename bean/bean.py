1343311c1e14390c002789e9d5996651fed28c62
izutsumi <76601426+izutsumi@users.noreply.github.com>
2021-06-17 02:38:24 -0400

    Update bean.py

--- bean/bean.py
+++ bean/bean.py
@@ -1,26 +1,51 @@
 import discord
 from discord.ext import commands
 
+
 class bean(commands.Cog):
+    """
+    Fake-ban a user
+    """
+
     def __init__(self, bot):
         self.bot = bot
         self.color = self.bot.main_color
-        
-    @commands.command(name="bean", aliases=['fban'])
-    async def bean(self, ctx, member: discord.Member = None, reason = None):
+
+    @commands.command(name="bean", aliases=["fban"])
+    async def bean(self, ctx, member: discord.Member = None, reason=None):
+        """
+        Bean a user of your choosing!
+        """
         await ctx.message.delete()
         if not member:
-            embed = discord.Embed(title="Error", description="Please provide a user to bean!", color=self.bot.error_color)
+            embed = discord.Embed(
+                title="Error",
+                description="Please provide a user to bean!",
+                color=self.bot.error_color,
+            )
             await ctx.send(embed=embed)
         else:
             if not reason:
-                embed = discord.Embed(title="Bean | case 420", description=f"{member.mention} Has been beaned!", color=self.color)
-                embed.set_thumbnail(url="https://cdn.discordapp.com/emojis/819994849838366771.png")
+                embed = discord.Embed(
+                    title="Bean | case 420",
+                    description=f"{member.mention} Has been beaned!",
+                    color=self.color,
+                )
+                embed.set_thumbnail(
+                    url="https://cdn.discordapp.com/emojis/819994849838366771.png"
+                )
                 await ctx.send(embed=embed)
             else:
-                embed = discord.Embed(title="Bean | case 69", description=f"{member.mention} Has been beaned for {reason}!", color=self.color)
-                embed.set_thumbnail(url="https://cdn.discordapp.com/emojis/819994849838366771.png")
-                await ctx.send(embed=embed)           
-        
+                embed = discord.Embed(
+                    title="Bean | case 69",
+                    description=f"{member.mention} Has been beaned for {reason}!",
+                    color=self.color,
+                )
+                embed.set_thumbnail(
+                    url="https://cdn.discordapp.com/emojis/819994849838366771.png"
+                )
+                await ctx.send(embed=embed)
+
+
 def setup(bot):
     bot.add_cog(bean(bot))
